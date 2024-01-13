b70b40a317fba314eee5321dd442ec746555c276
Craig Liesinger <craig.liesinger@gmail.com>
2024-01-13 22:57:25 +0100

    Update CameraViewManager.m

--- package/ios/CameraViewManager.m
+++ package/ios/CameraViewManager.m
@@ -52,6 +52,7 @@ RCT_EXPORT_VIEW_PROPERTY(resizeMode, NSString);
 // Camera View Events
 RCT_EXPORT_VIEW_PROPERTY(onError, RCTDirectEventBlock);
 RCT_EXPORT_VIEW_PROPERTY(onInitialized, RCTDirectEventBlock);
+RCT_EXPORT_VIEW_PROPERTY(onRecordingStart, RCTDirectEventBlock);
 RCT_EXPORT_VIEW_PROPERTY(onStarted, RCTDirectEventBlock);
 RCT_EXPORT_VIEW_PROPERTY(onStopped, RCTDirectEventBlock);
 RCT_EXPORT_VIEW_PROPERTY(onViewReady, RCTDirectEventBlock);
